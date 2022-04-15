fefc152886fa8fa45011d80c19bce78dab3cfd10
andreaminolfi <andrea@minolfi.it>
2022-04-15 08:14:01 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,31 @@ const baseUrl =
 })
 export class AppComponent {
   siscoperatori: SiscOperatore[] = [];
+  errors: string | null = null;
 
   constructor(private http: HttpClient) {}
   // invocato automaticamente dal framework dopo il costruttore
   ngOnInit(): void {
     this.getAll();
+    this.getNews();
   }
   // acquisisce un array di siscoperatori
   getAll(): void {
+    this.http.get<SiscOperatore[]>(`${baseUrl}/lista/noauth/full`).subscribe(
+      (result) => {
+        this.siscoperatori = result;
+      },
+      (error) => (this.errors = error)
+    );
+  }
+
+  getNews(): void {
     this.http
-      .get<SiscOperatore[]>(`${baseUrl}/lista/noauth/full`)
+      .get<any>(
+        `https://servizipubblicaamministrazione.it/servizi/geotecsueservice/api/news/seleziona/1`
+      )
       .subscribe((result) => {
         console.log(result);
-        this.siscoperatori = result;
       });
   }
 }
